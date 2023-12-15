947cf596af512b2306e102bb78975066a676fb59
thomasloux <thomas.loux4801@gmail.com>
2023-12-15 18:21:49 +0100

    unused file

--- init.sh
+++
@@ -1,9 +0,0 @@
-# Create env
-
-# Download dependencies
-python -m pip install -r requirements.txt
-
-# Download data
-kaggle competitions download -c copy-of-pscc-data-challenge
-
-# git clone (PSCC and 3dunet)
